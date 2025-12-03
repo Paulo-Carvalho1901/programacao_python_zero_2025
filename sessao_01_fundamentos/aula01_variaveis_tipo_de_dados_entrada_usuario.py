8d8d49342423a75ddf3007425a9c6e570e97dead
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-12-02 22:48:44 -0300

    feat: entendo os tipos primitivos

--- sessao_01_fundamentos/aula01_variaveis_tipo_de_dados_entrada_usuario.py
+++ sessao_01_fundamentos/aula01_variaveis_tipo_de_dados_entrada_usuario.py
@@ -2,13 +2,22 @@
 
 # Variaables
 
-primeiro_nome = 'Paulo'
-idade = 36
-altura = 1.78
-mensagens = 'Bom dia!'
+primeiro_nome = 'Paulo' # string
+idade = 36 # integer
+altura = 1.78 # float
+mensagens = 'Bom dia!' # string
+professor = False # Booleano
 
 
 print(primeiro_nome)
 print(idade)
 print(altura)
 print(mensagens)
+
+print()
+print('Conferindo os tipos de dados')
+
+print(type(primeiro_nome), primeiro_nome)
+print(type(idade), idade)
+print(type(altura), altura)
+print(type(professor), professor)
