d24e9a72904f631df97017a2c5819c7a4fb4d0b3
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-12-02 22:40:37 -0300

    feat: introduzindo variavel

---
+++ sessao_01_fundamentos/aula01_variaveis_tipo_de_dados_entrada_usuario.py
@@ -0,0 +1,14 @@
+# Variaveis e tipos de entrada dos usuário
+
+# Variaables
+
+primeiro_nome = 'Paulo'
+idade = 36
+altura = 1.78
+mensagens = 'Bom dia!'
+
+
+print(primeiro_nome)
+print(idade)
+print(altura)
+print(mensagens)
