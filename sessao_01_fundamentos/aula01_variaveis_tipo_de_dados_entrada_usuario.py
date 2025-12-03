23d13fece4b641bc650f973c9aeaf45538c277ee
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-12-02 22:56:03 -0300

    feat: trabalahndo com input()

--- sessao_01_fundamentos/aula01_variaveis_tipo_de_dados_entrada_usuario.py
+++ sessao_01_fundamentos/aula01_variaveis_tipo_de_dados_entrada_usuario.py
@@ -2,22 +2,32 @@
 
 # Variaables
 
-primeiro_nome = 'Paulo' # string
-idade = 36 # integer
-altura = 1.78 # float
-mensagens = 'Bom dia!' # string
-professor = False # Booleano
+# primeiro_nome = 'Paulo' # string
+# idade = 36 # integer
+# altura = 1.78 # float
+# mensagens = 'Bom dia!' # string
+# professor = False # Booleano
 
 
-print(primeiro_nome)
-print(idade)
-print(altura)
-print(mensagens)
+# print(primeiro_nome)
+# print(idade)
+# print(altura)
+# print(mensagens)
 
-print()
-print('Conferindo os tipos de dados')
+# print()
+# print('Conferindo os tipos de dados')
 
-print(type(primeiro_nome), primeiro_nome)
-print(type(idade), idade)
-print(type(altura), altura)
-print(type(professor), professor)
+# print(type(primeiro_nome), primeiro_nome)
+# print(type(idade), idade)
+# print(type(altura), altura)
+# print(type(professor), professor)
+
+# Interação com usuario
+
+# função
+# print() mostra na tela
+# type() mostra o tipo da variavel
+# input() solicita a o usuario algum valor (interage com usuario)
+
+nome = input('Digite um nome: ')
+print(nome)
