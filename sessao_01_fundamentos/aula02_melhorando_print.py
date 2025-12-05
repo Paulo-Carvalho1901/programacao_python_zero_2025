1c9632c4959e055123781e9680b753abf45f9428
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-12-05 01:59:10 -0300

    feat: utilizando f-string no print

---
+++ sessao_01_fundamentos/aula02_melhorando_print.py
@@ -0,0 +1,7 @@
+nome = input('Digite um nome: ')
+idade = input('Digite sua idade: ')
+
+print('Olá', nome + ', Você tem', idade, 'anos de idade')
+
+# f-string
+print(f'Olá {nome}, você tem {idade} anos de idade')
