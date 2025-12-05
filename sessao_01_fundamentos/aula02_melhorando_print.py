e4852cc05882cbaec06221cd3fb9039c95ae7347
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-12-05 02:05:44 -0300

    feat: add mais uma variavel fruta favorita

--- sessao_01_fundamentos/aula02_melhorando_print.py
+++ sessao_01_fundamentos/aula02_melhorando_print.py
@@ -1,7 +1,8 @@
 nome = input('Digite um nome: ')
 idade = input('Digite sua idade: ')
+fruta_favorita = input('Qual é sua fruta favorita: ')
 
 print('Olá', nome + ', Você tem', idade, 'anos de idade')
 
 # f-string
-print(f'Olá {nome}, você tem {idade} anos de idade')
+print(f'Olá {nome}, você tem {idade} anos de idade e sua fruta favotita é {fruta_favorita}')
