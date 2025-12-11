7163db7a2d09b30cb66715f1d026f1fd45978f6d
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-12-11 14:08:16 -0300

    feat: operadores aritimeticos

--- sessao_01_fundamentos/aula03_operadores_aritmeticos.py
+++ sessao_01_fundamentos/aula03_operadores_aritmeticos.py
@@ -1,12 +1,26 @@
-nome = input('Digite um nome: ')
-idade = int(input('Digite sua idade: '))
+# nome = input('Digite um nome: ')
+# idade = int(input('Digite sua idade: '))
 
 
 # f-string
-print(f'Olá {nome} daqui 5 anos você terá {idade + 5} amos de idade')
+# print(f'Olá {nome} daqui 5 anos você terá {idade + 5} amos de idade')
 
 # erro
 # TypeError: can only concatenate str (not "int") to str
 
 # print(type(nome))
 # print(type(idade))
+
+
+# Operadores aritimeticos
+num1 = 10
+num2 = 3
+
+
+print(num1 + num2) # soma
+print(num1 - num2) # subtração
+print(num1 * num2) # multiplicação
+print(num1 / num2) # divisao todo divisao simples retorna um float
+print(num1 // num2) # divisao inteira retona um int
+print(num1 % num2) # resto da divisao
+print(num1 ** num2) # potencia
