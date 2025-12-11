405bfb68c3af1c55babc257eff7ae6b0cd54a9b1
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-12-11 13:45:56 -0300

    feat: melhorando f-strings inserindo dados

--- sessao_01_fundamentos/aula02_melhorando_print.py
+++ sessao_01_fundamentos/aula02_melhorando_print.py
@@ -1,8 +1,12 @@
 nome = input('Digite um nome: ')
 idade = input('Digite sua idade: ')
-fruta_favorita = input('Qual é sua fruta favorita: ')
+# fruta_favorita = input('Qual é sua fruta favorita: ')
 
 print('Olá', nome + ', Você tem', idade, 'anos de idade')
 
+
 # f-string
-print(f'Olá {nome}, você tem {idade} anos de idade e sua fruta favotita é {fruta_favorita}')
+# print(f'Olá {nome}, você tem {idade} anos de idade e sua fruta favotita é {fruta_favorita}')
+print(f'Olá {nome} daqui 5 anos você terá {idade + 5} de idade')
+
+# TypeError: can only concatenate str (not "int") to str
